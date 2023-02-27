c5c9bfdd16d59f9452b52ec94cc8b8f02be7a126
v.kalmykov <v.kalmykov@tinkoff.ru>
2023-02-27 16:31:06 +0300

    special case mappings containing meta key for mac

--- lua/main/remap.lua
+++ lua/main/remap.lua
@@ -2,7 +2,7 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("n", "<leader>fp", function()
-    vim.cmd.Ex "~/.config/nvim/"
+	vim.cmd.Ex "~/.config/nvim/"
 end)
 
 vim.keymap.set("n", "<M-s>", vim.cmd.sp)
@@ -11,9 +11,9 @@ vim.keymap.set("n", "<M-q>", vim.cmd.q)
 
 -- move between windows with <M-hjkl>
 local function setMoveToWindow(direction)
-    vim.keymap.set("n", string.format("<M-%s>", direction), function()
-        vim.cmd.wincmd(direction)
-    end)
+	vim.keymap.set("n", string.format("<M-%s>", direction), function()
+		vim.cmd.wincmd(direction)
+	end)
 end
 
 setMoveToWindow('h')
@@ -24,3 +24,19 @@ setMoveToWindow('l')
 -- system clipboard
 vim.keymap.set("i", "<C-v>", "<Esc>\"+pa")
 vim.keymap.set("v", "<C-c>", "\"+y")
+
+-- for MacOs
+vim.keymap.set("n", "ß", vim.cmd.sp) -- <M-s>
+vim.keymap.set("n", "√", vim.cmd.vs) -- <M-v>
+vim.keymap.set("n", "œ", vim.cmd.q) -- <M-q>
+
+local function setMoveToWindowMac(symbol, direction)
+	vim.keymap.set("n", string.format("%s", symbol), function()
+		vim.cmd.wincmd(direction)
+	end)
+end
+
+setMoveToWindowMac('˙', 'h')
+setMoveToWindowMac('∆', 'j')
+setMoveToWindowMac('˚', 'k')
+setMoveToWindowMac('¬', 'l')
