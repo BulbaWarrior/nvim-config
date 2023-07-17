fd447553384e042133d5d75d087eb46602d54347
BulbaWarrior <vladkalm49@gmail.com>
2023-07-17 05:33:15 +0300

    tabs keybinds

--- lua/main/remap.lua
+++ lua/main/remap.lua
@@ -57,3 +57,16 @@ vim.keymap.set('n', '<M-x>', telescope.keymaps, {})
 --Lsp
 vim.keymap.set('n', '<leader>lc', '<CMD>e ~/.config/nvim/after/plugin/lsp.lua<CR>')
 vim.keymap.set('n', '<leader>li', '<CMD>LspInfo<CR>')
+
+-- Tabs and worksapces
+vim.keymap.set('n', '<Tab>n', '<CMD>tabnew<CR>')
+vim.keymap.set('n', '<Tab>]', '<CMD>tabnext<CR>')
+vim.keymap.set('n', '<Tab>[', '<CMD>tabprevious<CR>')
+vim.keymap.set('n', '<Tab>x', '<CMD>tabclose<CR>')
+
+local tabline_state = 1
+vim.keymap.set('n', '<Tab><Tab>', function()
+    vim.opt.showtabline = tabline_state == 1 and 0 or 1
+    tabline_state = tabline_state == 1 and 0 or 1
+    print(tabline_state)
+end)
